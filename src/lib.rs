2ba241644b59b6fbfea6dce2c1ff958751c1a2ca
Rostislav Raykov <z@zbrox.org>
2019-03-19 17:47:22 +0100

    Add official Bulgarian transliteration

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod gost779;
 mod passport2013;
 mod transliterator;
+mod bulgarian;
 
 #[cfg(test)]
 mod tests;
@@ -8,5 +9,6 @@ mod tests;
 pub use gost779::*;
 pub use passport2013::*;
 pub use transliterator::*;
+pub use bulgarian::*;
 
 pub type CharsMapping = Vec<(&'static str, &'static str)>;
